16dcacac9c05de7d1c1a3ea0960e2e88d6426581
Christopher Milne <christopher.milne@gmail.com>
2012-06-13 14:01:44 -0400

    major css edits, added layerslider

--- coned-landing-page-content.php
+++ coned-landing-page-content.php
@@ -1,3 +1,5 @@
+<link rel="stylesheet" href="../layerslider/css/layerslider.css" type="text/css">
+<link rel="stylesheet" href="common/layerslider/assets/css/style.css" type="text/css">
 <div id="big-audience-nav">
    <nav>
         <ul>
@@ -7,13 +9,14 @@
         </ul>
     </nav>
 </div>
-<div id="coned-messaging-area"><h1>hello world</h1></div>
+<div id="coned-messaging-area"></div>
+
 <div id="coned-search-bar">
     <h1><span>Explore</span> &#124 Continuing Education</h1>
     <form action="">
         <fieldset>
-            <input type="text">
-            <input type="submit">
+            <input type="text" placeholder="hello" class="coned-search-field">
+            <input type="submit" class="coned-search-submit">
         </fieldset>
     </form>
 </div>
@@ -110,4 +113,44 @@
             <li><a href="#">A-Z Course Listing</a></li>
         </ul>
     </aside>
+
+<div class="clearfix"></div>
 </div>
+
+
+
+
+
+
+<div id="coned-layerslider">
+<div id="dim">
+
+            <div id="layerslider" style="width: 80%;">
+                
+                <div class="ls-layer" rel="slidedelay: 3000; slidedirection: top;" style="background: url('common/layerslider/gallery/bgr2.jpg') center center">
+                    <img class="ls-s3" src="common/layerslider/gallery/l16.png" alt="sublayer" style="left: 50%; top: 50%">
+                    <img class="ls-s5" src="common/layerslider/gallery/l17.png" alt="sublayer" style="left: 20%; top: 20%">
+                    <img class="ls-s7" src="common/layerslider/gallery/l13.png" alt="sublayer" style="left: 80%; top: 80%">
+                </div>
+
+                <div class="ls-layer" rel="slidedelay: 3000;" style="background: url('common/layerslider/gallery/bgr1.jpg') center center">
+                    <div class="ls-s2" style="left: 50%; top: 50%">left: 50%, top: 50%</div>
+                    <div class="ls-s4" style="left: 20%; top: 20%">left: 20%, top: 20%</div>
+                    <div class="ls-s6" style="left: 80%; top: 80%">left: 80%, top: 80%</div>
+                </div>
+
+            </div>
+            
+        </div>
+</div>
+       <!-- <script src="../layerslider/jQuery/jquery-1.6.2.min.js" type="text/javascript"></script> -->
+        <script src="common/layerslider/layerslider/jQuery/jquery-easing-1.3.js" type="text/javascript"></script>
+        <script src="common/layerslider/layerslider/js/layerslider.kreaturamedia.jquery.js" type="text/javascript"></script>
+        <script type="text/javascript">
+            $(document).ready(function(){
+                $('#layerslider').layerSlider({
+                    skinsPath : '../layerslider/skins/',
+                    globalBGColor : '#333'
+                });
+            });     
+        </script>      
