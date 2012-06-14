9891eed025f647633ba1a6f6041cd8c5f9ef2127
Christopher Milne <christopher.milne@gmail.com>
2012-06-13 21:54:54 -0400

    created two page states for search

--- common/js/js2011/functions.js
+++ common/js/js2011/functions.js
@@ -10,10 +10,17 @@ var sheridan={homeInit:function(){
     			$('#big-audience-nav').hide();
     			$('#coned-messaging-area').hide();
 				$('#coned-landing-content').hide();
-				$('#coned-search-results').delay(750).fadeIn(750);
+				$('#coned-search-results').delay(750).fadeIn(1250);
 				return;
     		});
 
+    	});
+
+    	$('.coned-search-submit').click(function(){
+			$('#coned-search-results').fadeOut(50).hide();
+			$('#coned-landing-content').fadeIn(1250);
+			$('#coned-messaging-area').fadeIn(1250);
+    		$('#big-audience-nav').fadeIn(1250);
     	});
 
     });
