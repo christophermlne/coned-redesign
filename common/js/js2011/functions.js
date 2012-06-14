cb054315bbcb8ae0da8dbf2d26e68a07bc862dd7
Christopher Milne <christopher.milne@gmail.com>
2012-06-13 21:30:58 -0400

    search function--javascript prototype

--- common/js/js2011/functions.js
+++ common/js/js2011/functions.js
@@ -1,3 +1,20 @@
 var sheridan={homeInit:function(){
+    
     $(document).ready(function(){
-    });}};
+
+    	$('#coned-search-results').hide();
+
+    	$('.coned-search-field').focusin(function(){
+    		
+    		$(this).keydown(function(){
+    			$('#big-audience-nav').hide();
+    			$('#coned-messaging-area').hide();
+				$('#coned-landing-content').hide();
+				$('#coned-search-results').delay(750).fadeIn(750);
+				return;
+    		});
+
+    	});
+
+    });
+}};
