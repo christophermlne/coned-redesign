b8e57ddfdac45a2366c6420a589a7ed26994eb2e
Christopher Milne <christopher.milne@gmail.com>
2012-06-21 15:25:28 -0400

    set up

---
+++ coned-cluster-page-content.php
@@ -0,0 +1,53 @@
+<link rel="stylesheet" href="../layerslider/css/layerslider.css" type="text/css">
+<link rel="stylesheet" href="common/layerslider/assets/css/style.css" type="text/css">
+<div id="big-audience-nav">
+   <nav>
+        <ul>
+            <li class="active-tab"><a href="#">Prospective Students</a></li>
+            <li><a href="#">Newcomers</a></li>
+            <li><a href="#">Current Students</a></li>
+        </ul>
+    </nav>
+</div>
+
+<div id="coned-landing-content">
+    
+    <section id="coned-cluster-page-list">
+        <div></div>
+    </section>
+
+    <aside id="coned-aside">
+        <ul id="coned-sidebar-accr">
+             <li>
+                <a href="#">What's New</a>
+                <ul>
+                    <li><span>Title of News</span><p>Pellentesque habitant morbi</p><a href="#">read more</a>
+                    <li><span>Title of News</span><p>Pellentesque habitant morbi</p><a href="#">read more</a>
+                    <li><span>Title of News</span><p>Pellentesque habitant morbi</p><a href="#">read more</a>
+                    <li><span>Title of News</span><p>Pellentesque habitant morbi</p><a href="#">read more</a>                                    
+                </ul>
+            </li>
+            <li>
+                <a href="#">What's New</a>
+            </li>
+            <li>
+                <a href="#">What's New</a>
+            </li>
+        </ul>
+        <h3>See Also</h3>
+        <ul id="coned-sidebar-see-also">
+            <li><a href="#">General Info</a></li>
+            <li><a href="#">Fees</a></li>
+            <li><a href="#">Important Dates</a></li>
+            <li><a href="#">Information Sessions</a></li>
+            <li><a href="#">Policies</a></li>
+            <li><a href="#">How to Register</a></li>
+            <li><a href="#">Contact Us</a></li>
+            <li><a href="#">A-Z Course Listing</a></li>
+        </ul>
+    </aside>
+
+<div class="clearfix"></div>
+</div>
+
+
