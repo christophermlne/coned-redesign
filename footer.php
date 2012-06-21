dd2b3cf35ae5098d75f23913ab08012df6a3faae
Christopher Milne <christopher.milne@gmail.com>
2012-06-21 19:09:35 -0400

    adjusted .php includes
    adjusted .php includes to make it easier to work on the site

---
+++ footer.php
@@ -0,0 +1,88 @@
+               
+                        
+                </div>
+            </div>
+            <div id="footer-sher">
+                <div id="footer-wrapper">
+                    <div id="footer-nav">
+                        <ul>
+                            <li><a target="_blank" href="https://access.sheridaninstitute.ca/http://portal-am.sheridaninstitute.ca/amserver/UI/Login?gw=access.sheridaninstitute.ca&amp;org=o%3Dsheridanc.on.ca&amp;goto=http%3A%2F%2Fportal-p.sheridaninstitute.ca%3A80%2Fportal%2Fdt">
+                                AccessSheridan</a></li>
+                            <li><a target="_blank" href="https://vista.sheridaninstitute.ca/webct/entryPageIns.dowebct">
+                                SLATE</a></li>
+                            <li><a href="https://scdevcms.sheridancollege.ca/programs%20and%20courses/full-time%20programs/programs%20a-z%20index.aspx">
+                                Programs A-Z</a></li>
+                            <li><a href="http://it.sheridanc.on.ca/corpdir/" target="_blank">Faculty &amp; Staff
+                                Directory</a></li>
+                            <li><a href="https://scdevcms.sheridancollege.ca/about%20sheridan/campus%20locations.aspx">
+                                Maps &amp; Directions</a></li>
+                            <li><a href="https://scdevcms.sheridancollege.ca/about%20sheridan/contact%20%20us.aspx">
+                                Contact Us</a></li>
+                        </ul>
+                    </div>
+                    <div id="footer-campus-locations">
+                        <h2>
+                            Campus Locations</h2>
+                        <ul>
+                            <li>
+                                <h6>
+                                    Trafalgar Road Campus (Oakville)<a class="view-map" href="https://scdevcms.sheridancollege.ca/About%20Sheridan/Campus%20Locations/Trafalgar%20Road%20Campus.aspx">view
+                                        map</a></h6>
+                                <p>
+                                    1430 Trafalgar Road, Oakville, Ontario L6H 2L1</p>
+                            </li>
+                            <li>
+                                <h6>
+                                    Davis Campus (Brampton)<span class="view-map"><a class="view-map" href="https://scdevcms.sheridancollege.ca/About%20Sheridan/Campus%20Locations/Davis%20Campus.aspx">view
+                                        map</a></span></h6>
+                                <p>
+                                    7899 McLaughlin Rd., Brampton, Ontario L6Y 5H9</p>
+                            </li>
+                            <li>
+                                <h6>
+                                    Hazel McCallion Campus (Mississauga)<span class="view-map"><a class="view-map" href="http://www.sheridancollege.ca/About%20Sheridan/Campus%20Locations/Hazel%20McCallion%20Campus.aspx">view
+                                        map</a></span></h6>
+                                <p>
+                                    4180 Duke of York Boulevard, Mississauga, ON L5B 0G5</p>
+                            </li>
+                            <li>
+                                <h6>
+                                    Skills Training Centre (Oakville)<a class="view-map" href="https://scdevcms.sheridancollege.ca/About%20Sheridan/Campus%20Locations/Skills%20Training%20Centre.aspx">view
+                                        map</a></h6>
+                                <p>
+                                    407 Iroquois Shore Road, Oakville, Ontario L6H 1M3</p>
+                            </li>
+                        </ul>
+                    </div>
+                    <div id="footer-contact">
+                        <h2>
+                            Contact:</h2>
+                        <h6>
+                            Phone (Local Calling)</h6>
+                        <ul>
+                            <li>
+                                <p>
+                                    Oakville/Mississauga: 905-845-9430</p>
+                            </li>
+                            <li>
+                                <p>
+                                    Brampton/Mississauga: 905-459-7533</p>
+                            </li>
+                            <li>
+                                <p>
+                                    Hamilton: 905-681-4611</p>
+                            </li>
+                        </ul>
+                        <a href="https://scdevcms.sheridancollege.ca/Programs%20and%20Courses/Full-Time%20Programs/Programs%20A-Z%20Index/%7E/media/WF_03_MarCom_001/sheridan_privacy_policy_20101008.ashx">
+                            <h6>
+                                Privacy Policy</h6>
+                        </a>
+                    </div>
+                    <a href="#"></a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
