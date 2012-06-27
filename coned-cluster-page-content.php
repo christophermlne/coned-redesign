f9e3cd00bf207c8185a1af654dd4c7c816f5afea
marcel <marcelbernardou>
2012-06-27 15:32:56 -0400

    edits

--- coned-cluster-page-content.php
+++ coned-cluster-page-content.php
@@ -19,43 +19,96 @@
         <section id="coned-cluster-listing">
             <section id="cluster-items">
                 <h2>Accounting and Finance</h2>
-                <ul>
+                <ul class="list">
                     <li><a href="#">Canadian Securities</a></li>
                     <li><a href="#">Financial Planning</a></li>
                     <li><a href="#">Accounting and Finance - Other Related</a></li>
                     <li><a href="#">Canadian Payroll Association - Payroll Certification Programs</a></li>
                     <li><a href="#">Tax Accounting</a></li>
                  </ul>
-            </section>
-            <section id="cluster-items">
-                <h2>Accounting and Finance</h2>
-                <ul>
-                    <li><a href="#">Canadian Securities</a></li>
-                    <li><a href="#">Financial Planning</a></li>
-                    <li><a href="#">Accounting and Finance - Other Related</a></li>
-                    <li><a href="#">Canadian Payroll Association - Payroll Certification Programs</a></li>
-                    <li><a href="#">Tax Accounting</a></li>
-                 </ul>
-            </section>
-            <section id="cluster-items">
-                <h2>Accounting and Finance</h2>
-                <ul>
-                    <li><a href="#">Canadian Securities</a></li>
-                    <li><a href="#">Financial Planning</a></li>
-                    <li><a href="#">Accounting and Finance - Other Related</a></li>
-                    <li><a href="#">Canadian Payroll Association - Payroll Certification Programs</a></li>
-                    <li><a href="#">Tax Accounting</a></li>
-                 </ul>
-            </section>
-            <section id="cluster-items">
-                <h2>Accounting and Finance</h2>
-                <ul>
-                    <li><a href="#">Canadian Securities</a></li>
-                    <li><a href="#">Financial Planning</a></li>
-                    <li><a href="#">Accounting and Finance - Other Related</a></li>
-                    <li><a href="#">Canadian Payroll Association - Payroll Certification Programs</a></li>
-                    <li><a href="#">Tax Accounting</a></li>
-                 </ul>
+                 <ul class="list">
+                <li>
+                    <a target="_blank" href="http://sculptor.sheridanc.on.ca/webapp/wcs/stores/servlet/ProductDisplay?catalogId=10051&amp;storeId=10051&amp;productId=77307">
+                    Age Wave, Demographics and Business Impact<span class="online">AVAILABLE ONLINE</span><br /><span class="code">BUSM70014</span></a>
+                </li>
+                <li>
+                    <a target="_blank" href="http://sculptor.sheridanc.on.ca/webapp/wcs/stores/servlet/ProductDisplay?catalogId=10051&amp;storeId=10051&amp;productId=77334">
+                    Business Analysis Introduction<br /><span class="code">MGMT70012</span></a>
+                </li>
+                <li>
+                    <a target="_blank" href="http://sculptor.sheridanc.on.ca/webapp/wcs/stores/servlet/ProductDisplay?catalogId=10051&amp;storeId=10051&amp;productId=80167">
+                    Business Case Development, Acceptance Testing and Quality Assurance<br /><span class="code">MGMT70013</span></a>
+                </li>
+                <li>
+                    <a target="_blank" href="http://sculptor.sheridanc.on.ca/webapp/wcs/stores/servlet/ProductDisplay?catalogId=10051&amp;storeId=10051&amp;productId=47306">
+                    Business Communication<br /><span class="code">COMM12423</span></a>
+                    <!-- <div class="expanded-li">hello world</div> -->
+                </li>
+                <li>
+                    <a target="_blank" href="http://sculptor.sheridanc.on.ca/webapp/wcs/stores/servlet/ProductDisplay?catalogId=10051&amp;storeId=10051&amp;productId=79312">
+                    Business Facilitation and Communication<br /><span class="code">MGMT70014</span></a>
+                </li>
+                <li>
+                    <a target="_blank" href="http://sculptor.sheridanc.on.ca/webapp/wcs/stores/servlet/ProductDisplay?catalogId=10051&amp;storeId=10051&amp;productId=10834">
+                    Business Law - Distance Learning<span class="online">AVAILABLE ONLINE</span><br /><span class="code">BUSM70002</span></a>
+                </li>
+                <li>
+                    <a target="_blank" href="http://sculptor.sheridanc.on.ca/webapp/wcs/stores/servlet/ProductDisplay?catalogId=10051&amp;storeId=10051&amp;productId=43804">
+                    Business Mathematics 1<span class="online">AVAILABLE ONLINE</span><br /><span class="code">MATH16269</span></a>
+                </li>
+                <li>
+                    <a target="_blank" href="http://sculptor.sheridanc.on.ca/webapp/wcs/stores/servlet/ProductDisplay?catalogId=10051&amp;storeId=10051&amp;productId=43804">
+                    Business Mathematics 1<br /><span class="code">MATH16269</span></a>
+                </li>
+                <li>
+                    <a target="_blank" href="http://sculptor.sheridanc.on.ca/webapp/wcs/stores/servlet/ProductDisplay?catalogId=10051&amp;storeId=10051&amp;productId=80168">
+                    Business Process Modeling and Improvement<br /><span class="code">MGMT70015</span></a>
+                </li>
+                <li>
+                    <a target="_blank" href="http://sculptor.sheridanc.on.ca/webapp/wcs/stores/servlet/ProductDisplay?catalogId=10051&amp;storeId=10051&amp;productId=79313">
+                    Business Requirement Management<br /><span class="code">MGMT70016</span></a>
+                </li>
+                <li>
+                    <a target="_blank" href="http://sculptor.sheridanc.on.ca/webapp/wcs/stores/servlet/ProductDisplay?catalogId=10051&amp;storeId=10051&amp;productId=11610">
+                    Business Statistics, Advanced<br /><span class="code">MATH36901</span></a>
+                </li>
+                <li>
+                    <a target="_blank" href="http://sculptor.sheridanc.on.ca/webapp/wcs/stores/servlet/ProductDisplay?catalogId=10051&amp;storeId=10051&amp;productId=48551">
+                    Business Writing Strategies-Distance Learning<span class="online">AVAILABLE ONLINE</span><br /><span class="code">COMM70003</span></a>
+                </li>
+                <li>
+                    <a target="_blank" href="http://sculptor.sheridanc.on.ca/webapp/wcs/stores/servlet/ProductDisplay?catalogId=10051&amp;storeId=10051&amp;productId=10862">
+                    Business of Insurance<br /><span class="code">BUSM79806</span></a>
+                </li>
+                <li>
+                    <a target="_blank" href="http://sculptor.sheridanc.on.ca/webapp/wcs/stores/servlet/ProductDisplay?catalogId=10051&amp;storeId=10051&amp;productId=71051">
+                    Computer Applications for Business<br /><span class="code">APPL19402</span></a>
+                </li>
+                <li>
+                    <a target="_blank" href="http://sculptor.sheridanc.on.ca/webapp/wcs/stores/servlet/ProductDisplay?catalogId=10051&amp;storeId=10051&amp;productId=11517">
+                    Corporate and Business Law<br /><span class="code">LAWS26947</span></a>
+                </li>
+                <li>
+                    <a target="_blank" href="http://sculptor.sheridanc.on.ca/webapp/wcs/stores/servlet/ProductDisplay?catalogId=10051&amp;storeId=10051&amp;productId=88751">
+                    Desktop Publishing for Business<span class="online">AVAILABLE ONLINE</span><br /><span class="code">BUSM70022</span></a>
+                </li>
+                <li>
+                    <a target="_blank" href="http://sculptor.sheridanc.on.ca/webapp/wcs/stores/servlet/ProductDisplay?catalogId=10051&amp;storeId=10051&amp;productId=53212">
+                    Ethical Issues in Business - Distance Learning<span class="online">AVAILABLE ONLINE</span><br /><span class="code">BUSM70012</span></a>
+                </li>
+                <li>
+                    <a target="_blank" href="http://sculptor.sheridanc.on.ca/webapp/wcs/stores/servlet/ProductDisplay?catalogId=10051&amp;storeId=10051&amp;productId=43017">
+                    Law for Business Managers<br /><span class="code">LAWS70001</span></a>
+                </li>
+                <li>
+                    <a target="_blank" href="http://sculptor.sheridanc.on.ca/webapp/wcs/stores/servlet/ProductDisplay?catalogId=10051&amp;storeId=10051&amp;productId=89118">
+                    Small Business Management<span class="online">AVAILABLE ONLINE</span><br /><span class="code">MGMT70034</span></a>
+                </li>
+                <li>
+                    <a target="_blank" href="http://sculptor.sheridanc.on.ca/webapp/wcs/stores/servlet/ProductDisplay?catalogId=10051&amp;storeId=10051&amp;productId=54304">
+                    Supervisory Skills for Business and Industry - Distance Learning<span class="online">AVAILABLE ONLINE</span><br /><span class="code">BUSM79004</span></a>
+                </li>
+            </ul>
             </section>
         </section>
     
