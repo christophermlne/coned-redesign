b0c69a1b6ec5a7305eb7d268f042952d8aeae51a
Christina Holly <tinacious.design@gmail.com>
2012-07-05 15:41:39 -0400

    search.php include

--- coned-cluster-page-content.php
+++ coned-cluster-page-content.php
@@ -73,16 +73,7 @@
 
 
 <aside id="coned-aside" class="not-landing">
-    <div id="coned-sidebar-search">
-            <h1><span>Explore</span> Continuing Education</h1>
-            <form action="">
-            <fieldset>
-                <input type="text" placeholder="Find Programs &amp; Courses" class="coned-search-field">
-                 <button type="button" name="Submit" class="coned-search-submit"><span>Search</span></button>
-            </fieldset>
-            </form>
-            <a href="#" id="close-search">Close</a>
-     </div>
+    <?php include('search.php'); ?>
      <div class="clearfix"></div>
        <ul id="coned-sidebar-see-also">
                 <li><a href="#">General Info</a></li>
