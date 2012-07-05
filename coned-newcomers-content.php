430d2ae680765644dd1443c01968fea8daf04836
Christina Holly <tinacious.design@gmail.com>
2012-07-05 14:27:29 -0400

    sidebar search fix
    it doesn't expand and destroy the layout anymore when you start typing

--- coned-newcomers-content.php
+++ coned-newcomers-content.php
@@ -65,7 +65,7 @@
                 <h1><span>Explore</span> Continuing Education</h1>
                 <form action="">
                 <fieldset>
-                    <input type="text" placeholder="Find Programs &amp; Courses" class="coned-search-field">
+                    <input type="text" placeholder="Find Programs &amp; Courses" class="coned-search-field-sm">
                      <button type="button" name="Submit" class="coned-search-submit"><span>Search</span></button>
                 </fieldset>
                 </form>
