00b28785d9a0c479efe62e3740f33a0ede6a4727
Christina Holly <tinacious.design@gmail.com>
2012-07-06 10:53:33 -0400

    clean up
    brought adelle back from rolling in the deep, smoothed out the font on
    webkit; cleaned up margins and padding randomly throughout;
    cluster-page and newcomers stylistically match more (title, image,
    headers, margins); adjusted paragraph line-height to increase
    readability; fixed the clusters on the landing page (they were pouring
    out of the side).

--- coned-cluster-page-content.php
+++ coned-cluster-page-content.php
@@ -3,90 +3,91 @@
 
 
 <section id="coned-title">
-    <div class="crumbs">
-        <ul>
-            <li><a href="#">Home <span>/</span></a></li>
-            <li><a href="#">Programs and Courses <span>/</span> </a></li>
-            <li><a href="#">Programs </a></li>
-        </ul>
-    </div>
-    <h1>Continuing Education - Business Programs</h1>
+	<div class="crumbs">
+		<ul>
+			<li><a href="#">Home <span>/</span></a></li>
+			<li><a href="#">Programs and Courses <span>/</span> </a></li>
+			<li><a href="#">Programs </a></li>
+		</ul>
+	</div>
+	<h1>Continuing Education - Business Programs</h1>
 </section>
 
 
 <div id="coned-landing-content" class="not-landing">
-     <section id="coned-cluster-featured">
-        <!--image/campaign for cluster here</h1>--></section>
-        <section id="coned-intro"><p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p></section>
-         <section id="coned-cluster-listing">
-            <h2>Choose a program:</h2>
-            <section class="cluster-items">
-                <h3>Accounting and Finance</h3>
-                <ul class="list">
-                    <li><a href="#">Canadian Securities</a></li>
-                    <li><a href="#">Financial Planning</a></li>
-                    <li><a href="#">Accounting and Finance - Other Related</a></li>
-                    <li><a href="#">Canadian Payroll Association - Payroll Certification Programs</a></li>
-                    <li><a href="#">Tax Accounting</a></li>
-                    <li><a href="#">Accounting and Finance - Other Related</a></li>
-                    <li><a href="#">Canadian Payroll Association - Payroll Certification Programs</a></li>
-                    <li><a href="#">Tax Accounting</a></li>
-                 </ul>
-                 
-            </section>
-            <section class="cluster-items">
-                <h3>Accounting and Finance</h3>
-                <ul class="list">
-                    <li><a href="#">Canadian Securities</a></li>
-                    <li><a href="#">Financial Planning</a></li>
-                    <li><a href="#">Accounting and Finance - Other Related</a></li>
-                    <li><a href="#">Canadian Payroll Association - Payroll Certification Programs</a></li>
-                    <li><a href="#">Tax Accounting</a></li>
-                 </ul>
-                 
-            </section>
-            <section class="cluster-items">
-                <h3>Accounting and Finance</h3>
-                <ul class="list">
-                    <li><a href="#">Canadian Securities</a></li>
-                    <li><a href="#">Financial Planning</a></li>
-                    <li><a href="#">Accounting and Finance - Other Related</a></li>
-                    <li><a href="#">Canadian Payroll Association - Payroll Certification Programs</a></li>
-                    <li><a href="#">Tax Accounting</a></li>
-                 </ul>
-                 
-            </section>
-            <section class="cluster-items">
-                <h3>Accounting and Finance</h3>
-                <ul class="list">
-                    <li><a href="#">Canadian Securities</a></li>
-                    <li><a href="#">Financial Planning</a></li>
-                    <li><a href="#">Accounting and Finance - Other Related</a></li>
-                    <li><a href="#">Canadian Payroll Association - Payroll Certification Programs</a></li>
-                    <li><a href="#">Tax Accounting</a></li>
-                 </ul>
-                 
-            </section>
-        </section>
-    
+	 <section id="coned-cluster-featured">
+		<!--image/campaign for cluster here</h1>-->
+	</section>
+		<section id="coned-intro"><p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p></section>
+		 <section id="coned-cluster-listing">
+			<h2>Choose a program:</h2>
+			<section class="cluster-items">
+				<h3>Accounting and Finance</h3>
+				<ul class="list">
+					<li><a href="#">Canadian Securities</a></li>
+					<li><a href="#">Financial Planning</a></li>
+					<li><a href="#">Accounting and Finance - Other Related</a></li>
+					<li><a href="#">Canadian Payroll Association - Payroll Certification Programs</a></li>
+					<li><a href="#">Tax Accounting</a></li>
+					<li><a href="#">Accounting and Finance - Other Related</a></li>
+					<li><a href="#">Canadian Payroll Association - Payroll Certification Programs</a></li>
+					<li><a href="#">Tax Accounting</a></li>
+				 </ul>
+				 
+			</section>
+			<section class="cluster-items">
+				<h3>Accounting and Finance</h3>
+				<ul class="list">
+					<li><a href="#">Canadian Securities</a></li>
+					<li><a href="#">Financial Planning</a></li>
+					<li><a href="#">Accounting and Finance - Other Related</a></li>
+					<li><a href="#">Canadian Payroll Association - Payroll Certification Programs</a></li>
+					<li><a href="#">Tax Accounting</a></li>
+				 </ul>
+				 
+			</section>
+			<section class="cluster-items">
+				<h3>Accounting and Finance</h3>
+				<ul class="list">
+					<li><a href="#">Canadian Securities</a></li>
+					<li><a href="#">Financial Planning</a></li>
+					<li><a href="#">Accounting and Finance - Other Related</a></li>
+					<li><a href="#">Canadian Payroll Association - Payroll Certification Programs</a></li>
+					<li><a href="#">Tax Accounting</a></li>
+				 </ul>
+				 
+			</section>
+			<section class="cluster-items">
+				<h3>Accounting and Finance</h3>
+				<ul class="list">
+					<li><a href="#">Canadian Securities</a></li>
+					<li><a href="#">Financial Planning</a></li>
+					<li><a href="#">Accounting and Finance - Other Related</a></li>
+					<li><a href="#">Canadian Payroll Association - Payroll Certification Programs</a></li>
+					<li><a href="#">Tax Accounting</a></li>
+				 </ul>
+				 
+			</section>
+		</section>
+	
 </div>
 
 
 <aside id="coned-aside" class="not-landing">
 
-    <?php include('search.php'); ?>
-    
-     <div class="clearfix"></div>
-       <ul id="coned-sidebar-see-also">
-                <li><a href="#">General Info</a></li>
-                <li><a href="#">Fees</a></li>
-                <li><a href="#">Important Dates</a></li>
-                <li><a href="#">Information Sessions</a></li>
-                <li><a href="#">Policies</a></li>
-                <li><a href="#">How to Register</a></li>
-                <li><a href="#">Contact Us</a></li>
-                <li><a href="#">A-Z Course Listing</a></li>
-            </ul>
+	<?php include('search.php'); ?>
+	
+	 <div class="clearfix"></div>
+	   <ul id="coned-sidebar-see-also">
+				<li><a href="#">General Info</a></li>
+				<li><a href="#">Fees</a></li>
+				<li><a href="#">Important Dates</a></li>
+				<li><a href="#">Information Sessions</a></li>
+				<li><a href="#">Policies</a></li>
+				<li><a href="#">How to Register</a></li>
+				<li><a href="#">Contact Us</a></li>
+				<li><a href="#">A-Z Course Listing</a></li>
+			</ul>
 
 
 </aside>
--- coned-newcomers-content.php
+++ coned-newcomers-content.php
@@ -5,124 +5,136 @@
 
 <!--<div id="big-audience-nav">
    <nav>
-        <ul>
-            <li class="active-tab"><a href="#">Prospective Students</a></li>
-            <li><a href="#">Newcomers</a></li>
-            <li><a href="#">Current Students</a></li>
-        </ul>
-    </nav>
+		<ul>
+			<li class="active-tab"><a href="#">Prospective Students</a></li>
+			<li><a href="#">Newcomers</a></li>
+			<li><a href="#">Current Students</a></li>
+		</ul>
+	</nav>
 </div>-->
 
+<section id="coned-title">
+	<div class="crumbs">
+		<ul>
+			<li><a href="#">Home <span>/</span></a></li>
+			<li><a href="#">Programs and Courses <span>/</span> </a></li>
+			<li><a href="#">Programs </a></li>
+		</ul>
+	</div>
+	<h1>Welcome to Canada</h1>
+</section>
+
 <div id="coned-landing-content" class="not-landing">
-    <section id="coned-secondary-featured"><!--Imagery to come--></section>
-    <section id="intro-newcom">
-            <h1>Welcome to Canada</h1>
-            <div class="intro-text"><p>Pellentesque habitant morbi tristique senectus et netus et malesuada 
-            fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p><p>Pellentesque habitant morbi tristique senectus et netus et malesuada 
-            est. Mauris placerat eleifend leo.</p></div>
-            <div class="call-out"><h3>Information Sessions</h3><p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.<br> <br><a href="#">Click here to sign up for</a></p></div>
-            
-     </section>
-      <section id="way-find">
-            <section id="way-find-item">
-                <h2>Job Preperation</h2>
-                <div class="way-find-desc">
-                    <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
-                 </div>
-                <div class="way-find-links">
-                    <h3>Title for the links</h3>
-                    <p>this is the description</p>
-                <ul class="list-2">
-                    <li><a href="#">Canadian Securities<br /><span class="desc">this is the small text</span></a></li>
-                    <li><a href="#">Financial Planning<br /><span class="desc">this is the small text</span></a></li>
-                    <li><a href="#">Accounting and Finance - Other Related<br /><span class="desc">this is the small text</span></a></li>
-                 </ul></div>
-            </section>
-            <section id="way-find-item">
-                <h2>Language Training</h2>
-                <div class="way-find-desc">
-                    <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
-                    <p>Pellenstas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
-                    </div>
-                <div class="way-find-links">
-                <ul class="list-2">
-                    <li><a href="#">Canadian Securities<br /><span class="desc">this is the small text</span></a></li>
-                    <li><a href="#">Financial Planning<br /><span class="desc">this is the small text</span></a></li>
-                    <li><a href="#">Accounting and Finance - Other Related<br /><span class="desc">this is the small text</span></a></li>
-                    <li><a href="#">Canadian Payroll Association - Payroll Certification Programs<br /><span class="desc">this is the small text</span></a></li>
-                    <li><a href="#">Tax Accounting<br /><span class="desc">this is the small text</span></a></li>
-                    <li><a href="#">Accounting and Finance - Other Related<br /><span class="desc">this is the small text</span></a></li>
-                </ul></div>
-            </section>
+		
+	<section id="coned-cluster-featured"><!--Imagery to come--></section>
+	
+	<section class="newcomers-intro">
+		<div class="intro-text"><p>Pellentesque habitant morbi tristique senectus et netus et malesuada 
+	fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p><p>Pellentesque habitant morbi tristique senectus et netus et malesuada 
+	est. Mauris placerat eleifend leo.</p></div>
+		<div class="call-out"><h3>Information Sessions</h3><p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.<br> <br><a href="#">Click here to sign up for</a></p></div>
+	</section>
+
+	  <section id="way-find">
+			<section id="way-find-item">
+				<h2>Job Preperation</h2>
+				<div class="way-find-desc">
+					<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+				 </div>
+				<div class="way-find-links">
+					<h3>Title for the links</h3>
+					<p>this is the description</p>
+				<ul class="list-2">
+					<li><a href="#">Canadian Securities<br /><span class="desc">this is the small text</span></a></li>
+					<li><a href="#">Financial Planning<br /><span class="desc">this is the small text</span></a></li>
+					<li><a href="#">Accounting and Finance - Other Related<br /><span class="desc">this is the small text</span></a></li>
+				 </ul></div>
+			</section>
+			<section id="way-find-item">
+				<h2>Language Training</h2>
+				<div class="way-find-desc">
+					<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+					<p>Pellenstas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+					</div>
+				<div class="way-find-links">
+				<ul class="list-2">
+					<li><a href="#">Canadian Securities<br /><span class="desc">this is the small text</span></a></li>
+					<li><a href="#">Financial Planning<br /><span class="desc">this is the small text</span></a></li>
+					<li><a href="#">Accounting and Finance - Other Related<br /><span class="desc">this is the small text</span></a></li>
+					<li><a href="#">Canadian Payroll Association - Payroll Certification Programs<br /><span class="desc">this is the small text</span></a></li>
+					<li><a href="#">Tax Accounting<br /><span class="desc">this is the small text</span></a></li>
+					<li><a href="#">Accounting and Finance - Other Related<br /><span class="desc">this is the small text</span></a></li>
+				</ul></div>
+			</section>
 </div>
 
 
 <aside id="coned-aside" class="not-landing">
 
-            <?php include('search.php'); ?>
-            
-             <div class="clearfix"></div> 
-           <!-- <ul id="coned-sidebar-see-also">
-                <li id="news-hightlight">
-                    <h3>What's New</h3>
-                    <ul>
-                        <li class="news-item">
-                            <h4>New coned clusters announced!</h4>
-                            <p>
-                                Omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.
-                            </p>
-                            <a href="#">read more</a>
-                        </li>
-                        <li class="news-item">
-                            <h4>New coned clusters announced!</h4>
-                            <p>
-                                Omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.
-                            </p>
-                            <a href="#">read more</a>
-                        </li>
-                        <li class="news-item">
-                            <h4>New coned clusters announced!</h4>
-                            <p>
-                                Omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.
-                            </p>
-                            <a href="#">read more</a>
-                        </li>
-                        
-                    </ul>
-                </li>
-                <li><a href="#">General Info</a></li>
-                <li><a href="#">Fees</a></li>
-                <li><a href="#">Important Dates</a></li>
-                <li><a href="#">Information Sessions</a></li>
-                <li><a href="#">Policies</a></li>
-                <li><a href="#">How to Register</a></li>
-                <li><a href="#">Contact Us</a></li>
-            </ul>-->
+			<?php include('search.php'); ?>
+			
+			 <div class="clearfix"></div> 
+		   <!-- <ul id="coned-sidebar-see-also">
+				<li id="news-hightlight">
+					<h3>What's New</h3>
+					<ul>
+						<li class="news-item">
+							<h4>New coned clusters announced!</h4>
+							<p>
+								Omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.
+							</p>
+							<a href="#">read more</a>
+						</li>
+						<li class="news-item">
+							<h4>New coned clusters announced!</h4>
+							<p>
+								Omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.
+							</p>
+							<a href="#">read more</a>
+						</li>
+						<li class="news-item">
+							<h4>New coned clusters announced!</h4>
+							<p>
+								Omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.
+							</p>
+							<a href="#">read more</a>
+						</li>
+						
+					</ul>
+				</li>
+				<li><a href="#">General Info</a></li>
+				<li><a href="#">Fees</a></li>
+				<li><a href="#">Important Dates</a></li>
+				<li><a href="#">Information Sessions</a></li>
+				<li><a href="#">Policies</a></li>
+				<li><a href="#">How to Register</a></li>
+				<li><a href="#">Contact Us</a></li>
+			</ul>-->
 
-            <?php include('news.php'); ?>
+			<?php include('news.php'); ?>
 
-          <script>
-            $(document).ready(function() {
-                $('#news_slider').bjqs({
-                  'animation' : 'slide',
-                  'width' : 280,
-                  'height' : 130
-                });
-              });
-        </script>
+		  <script>
+			$(document).ready(function() {
+				$('#news_slider').bjqs({
+				  'animation' : 'slide',
+				  'width' : 280,
+				  'height' : 130
+				});
+			  });
+		</script>
 
 		<?php include('twitter.php'); ?>
 
 
-            <ul id="coned-sidebar-see-also">
-                <li><a href="#">General Info</a></li>
-                <li><a href="#">Fees</a></li>
-                <li><a href="#">Important Dates</a></li>
-                <li><a href="#">Information Sessions</a></li>
-                <li><a href="#">Policies</a></li>
-                <li><a href="#">How to Register</a></li>
-                <li><a href="#">Contact Us</a></li>
-            </ul>
+			<ul id="coned-sidebar-see-also">
+				<li><a href="#">General Info</a></li>
+				<li><a href="#">Fees</a></li>
+				<li><a href="#">Important Dates</a></li>
+				<li><a href="#">Information Sessions</a></li>
+				<li><a href="#">Policies</a></li>
+				<li><a href="#">How to Register</a></li>
+				<li><a href="#">Contact Us</a></li>
+			</ul>
 
 
 </aside>
