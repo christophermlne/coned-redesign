58346291a1dd233656e322074a5cbcdd510b487d
bernamar <marcelbernardou@gmail.com>
2012-07-09 14:30:37 -0400

    edits

--- coned-current-stu-content.php
+++ coned-current-stu-content.php
@@ -20,61 +20,53 @@
 
 
 <div id="coned-landing-content" class="not-landing">
+
+
 	 <section id="coned-cluster-featured">
 		<!--image/campaign for cluster here</h1>-->
 	</section>
-		<section id="coned-intro"><p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p></section>
-		 <section id="coned-cluster-listing">
-			<h2>Choose a program:</h2>
-			<section class="cluster-items">
-				<h3>Accounting and Finance</h3>
-				<ul class="list">
-					<li><a href="#">Canadian Securities</a></li>
-					<li><a href="#">Financial Planning</a></li>
-					<li><a href="#">Accounting and Finance - Other Related</a></li>
-					<li><a href="#">Canadian Payroll Association - Payroll Certification Programs</a></li>
-					<li><a href="#">Tax Accounting</a></li>
-					<li><a href="#">Accounting and Finance - Other Related</a></li>
-					<li><a href="#">Canadian Payroll Association - Payroll Certification Programs</a></li>
-					<li><a href="#">Tax Accounting</a></li>
-				 </ul>
-				 
-			</section>
-			<section class="cluster-items">
-				<h3>Accounting and Finance</h3>
-				<ul class="list">
-					<li><a href="#">Canadian Securities</a></li>
-					<li><a href="#">Financial Planning</a></li>
-					<li><a href="#">Accounting and Finance - Other Related</a></li>
-					<li><a href="#">Canadian Payroll Association - Payroll Certification Programs</a></li>
-					<li><a href="#">Tax Accounting</a></li>
-				 </ul>
-				 
-			</section>
-			<section class="cluster-items">
-				<h3>Accounting and Finance</h3>
-				<ul class="list">
-					<li><a href="#">Canadian Securities</a></li>
-					<li><a href="#">Financial Planning</a></li>
-					<li><a href="#">Accounting and Finance - Other Related</a></li>
-					<li><a href="#">Canadian Payroll Association - Payroll Certification Programs</a></li>
-					<li><a href="#">Tax Accounting</a></li>
-				 </ul>
-				 
-			</section>
-			<section class="cluster-items">
-				<h3>Accounting and Finance</h3>
-				<ul class="list">
-					<li><a href="#">Canadian Securities</a></li>
-					<li><a href="#">Financial Planning</a></li>
-					<li><a href="#">Accounting and Finance - Other Related</a></li>
-					<li><a href="#">Canadian Payroll Association - Payroll Certification Programs</a></li>
-					<li><a href="#">Tax Accounting</a></li>
-				 </ul>
-				 
-			</section>
-		</section>
-	
+
+
+<section class="currentstudents-intro">
+		<div class="intro-text"><p>We are proud to call ourselves Leaders in Lifelong Learning. Our courses incorporate up-to-date technologies and innovative ideas. Our expanded online offerings provide additional flexibility to learn in the environment of your choice. We welcome you to take advantage of valuable opportunities to collaborate, network with peers and learn from fellow students. Continue building your success at Sheridan. Personal or professional, the possibilities are truly endless.</p>
+		</div>
+		
+
+		<div class="local-nav">
+			<ul>
+				<li><a href="#">MyStudent<br /> Centre</a></li>
+				<li><a href="#">Final Exams<br /> Book Online</a></li>
+				<li><a href="#">Request<br /> Transcript</a></li>
+				<li><a href="#">Academic<br /> Policies</a></li>
+			</ul>
+		</div>
+		
+		<div class="helpful-links">
+			<h3>Helpful Links</h3>
+			<ul>
+				<li><a href="#">Book Online Final Exam</a></li>
+				<li><a href="#">Student Make Up Test Policy</a></li>
+				<li><a href="#">Student Services and Resources</a></li>
+				<li><a href="#">MyStudent Center</a></li>
+				<li><a href="#">e-Store</a></li>
+				<li><a href="#">FAQs for Online Learners</a></li>
+				<li><a href="#">Ready to Register</a></li>
+				<li><a href="#">Transcripts</a></li>
+				<li><a href="#">Advanced Standing</a></li>
+				<li><a href="#">Prior Learning Assessment Recognition (PLAR)</a></li>
+				<li><a href="#">Graduation Requirements</a></li>
+			</ul>
+		</div>
+		<div class="call-out-2">
+			<h3>Information Sessions</h3>
+			<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. malesuada fames ac turpis egestas. malesuada fames ac turpis egestas.<br> 
+				<br><a href="#">Click here to sign up for</a>
+			</p>
+		</div>
+
+
+</section>
+
 </div>
 
 
