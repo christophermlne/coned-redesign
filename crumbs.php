15941494ca643ae44cd418ed5b3e44abebc1e264
bernamar <marcelbernardou@gmail.com>
2012-07-17 10:00:37 -0400

    crumbs updated

--- crumbs.php
+++ crumbs.php
@@ -1,6 +1,6 @@
 <section id="crumbs">
 		<ul>
-			<li><a href="#" title="Home"><div><img src="common/coned_images/crumbs-house.png" alt="Home" /></div></a></li>
+			<li><a href="#">Continuing &amp Professional Studies</a></li>
 			<li><a href="#">Programs and Courses</a></li>
 			<li><a href="#">Programs</a></li>
 		</ul>
